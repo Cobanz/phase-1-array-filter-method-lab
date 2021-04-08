03ffc668e9c1595ce23be4eab181f794e9a157b7
Charles Grant Nichols <grant@inkdots.com>
2021-04-08 18:44:56 -0500

    commit tree c1d204740ebc816cbe78010e0d4f949cc6211aac

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter( match =>
+         match.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(drivers, charater) {
+    return drivers.filter( match =>
+        match.startsWith(charater))
+}
+
+
+
+
+function matchName(drivers) {
+    return drivers.filter( match =>
+         match.name === "Bobby" )}
